0adb2310ddbbe15d753f37f49ccd25482d3d3113
Paul Yi <29391424+paulcyi@users.noreply.github.com>
2024-09-18 05:44:00 -0400

    Create Xcode project

---
+++ Vet Health/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vet Health
+//
+//  Created by Paul Yi on 9/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vet Health/Vet_HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vet_HealthApp.swift
+//  Vet Health
+//
+//  Created by Paul Yi on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Vet_HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
